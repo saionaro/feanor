632fa4f7e2374d6cedcad4a56094a73077e21637
Saionaro <shibakow@gmail.com>
2019-05-12 13:49:17 +0300

    Yarn init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 const yargs = require("yargs");
+const spawn = require("cross-spawn");
+const path = require("path");
 const fs = require("fs");
 const util = require("util");
 
 const mkdir = util.promisify(fs.mkdir);
 
-async function init(argv) {
+function handleProcessExit(resolve, reject, command, args) {
+  return code => {
+    if (code !== 0) {
+      return reject({
+        command: `${command} ${args.join(" ")}`
+      });
+    }
+    resolve();
+  };
+}
+
+function init(root) {
+  return new Promise((resolve, reject) => {
+    const command = "yarn";
+    const args = ["--cwd", root, "init", "-y"];
+
+    const process = spawn(command, args, { stdio: "inherit" });
+
+    process.on("close", handleProcessExit(resolve, reject, command, args));
+  });
+}
+
+function install(root, deps = []) {
+  return new Promise((resolve, reject) => {
+    const command = "yarn";
+    const args = ["--cwd", root, "add", "--exact", ...deps];
+
+    const process = spawn(command, args, { stdio: "inherit" });
+
+    process.on("close", handleProcessExit(resolve, reject, command, args));
+  });
+}
+
+async function createProject(argv) {
   if (argv.name) {
     await mkdir(argv.name);
+    await mkdir(`${argv.name}/src`);
+    const projectRoot = path.join(process.cwd(), argv.name);
+
+    await init(projectRoot);
+    await install(projectRoot, ["eslint"]);
   }
 }
 
@@ -20,5 +60,5 @@ yargs.usage("Usage: $0 <command> [options]").command({
       type: "string"
     });
   },
-  handler: init
+  handler: createProject
 }).argv;
