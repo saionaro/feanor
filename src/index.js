c3021080438274d1337bb9738074d2e4cef4e2d4
Saionaro <shibakow@gmail.com>
2019-05-12 13:19:06 +0300

    Added project initialization command

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-let helloWorld = "Hello";
-helloWorld += " world";
+const yargs = require("yargs");
+const fs = require("fs");
+const util = require("util");
 
-helloWorld;
+const mkdir = util.promisify(fs.mkdir);
+
+async function init(argv) {
+  if (argv.name) {
+    await mkdir(argv.name);
+  }
+}
+
+yargs.usage("Usage: $0 <command> [options]").command({
+  command: "init <name>",
+  desc: "Initialize new project",
+  aliases: ["i"],
+  builder: yargs => {
+    yargs.positional("name", {
+      describe: "Project name to init",
+      type: "string"
+    });
+  },
+  handler: init
+}).argv;
